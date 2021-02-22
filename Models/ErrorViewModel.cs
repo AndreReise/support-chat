b11edaa586d9d293ac0df3f7b159d945b2d6b3a1
Andrew Babanin <39223191+AndreReise@users.noreply.github.com>
2021-02-22 12:12:16 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace support_chat.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
