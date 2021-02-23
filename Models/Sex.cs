7fe35f9dbae16d7b1de153be3a2c30fc98ac2341
Viktor <volynets.viktor@gmail.com>
2021-02-23 15:18:24 +0200

    update volynets onto master

---
+++ Models/Sex.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace TechnicalSupport.Models
+{
+    public partial class Sex
+    {
+        public Sex()
+        {
+            Employees = new HashSet<Employee>();
+            Users = new HashSet<Client>();
+        }
+
+        public int SexId { get; set; }
+        public string Sex1 { get; set; }
+
+        public virtual ICollection<Employee> Employees { get; set; }
+        public virtual ICollection<Client> Users { get; set; }
+    }
+}
