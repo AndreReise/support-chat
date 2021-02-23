b074805fca221e191a264c509331114dbca30962
Viktor <volynets.viktor@gmail.com>
2021-02-23 11:38:53 +0200

    Added dialog registration

--- TechnicalSupport/AutoChat/AutoDialog.cs
+++ TechnicalSupport/AutoChat/AutoDialog.cs
@@ -14,6 +14,8 @@ namespace TechnicalSupport
         delegate Message DialogMetod (Message mes);
         private Dictionary<Guid, int> clientDictionary;
         private Dictionary<Guid, int> dialogOneState;
+        private Dictionary<Guid, int> dialogTwoState;
+        Dictionary<Guid, Dictionary<string, string>> UserDataDictionary;
         private ChatContext _context;
 
         enum Dialogs
@@ -31,13 +33,17 @@ namespace TechnicalSupport
 
 
         string [] textArrDialogOne = { "Нове бронювання", "Змінити бронювання"};
-      
+        string[] textArrDialogRegister = { "Для регістрації введіть номер бронювання", " Оберіть номер місця в діапазоні 0-44", "Ви зареестровані!" };
+        string[] arrNumberBooking = {"BA5555","BB444","BC3333", "AA1111" };
+
+
         public AutoDialog(ChatContext context)
         {
             _context = context;
             clientDictionary = new Dictionary<Guid, int>();
             dialogOneState = new Dictionary<Guid, int>();
-
+            dialogTwoState = new Dictionary<Guid, int>();
+            UserDataDictionary = new Dictionary<Guid, Dictionary<string, string>>();
             DialogName = new Dictionary<Dialogs, string>() {
                 { Dialogs.Booking, "Бронювання квитків" },
                 { Dialogs.Register, "Онлайн реєстрація" },
@@ -155,12 +161,165 @@ namespace TechnicalSupport
         {
 
 
+            
+            if (dialogTwoState.ContainsKey(mes.DialogId))
+            {
+
+                var state = dialogTwoState[mes.DialogId];
+
+                if (textArrDialogRegister.Length > state && state > 0)
+                {
+                    bool valid = false;
+                    switch (state)
+                    {
+                        case 1:
+                            valid = ValidationOne(mes);
+                            break;
+                        case 2:
+                            valid = ValidationTwo(mes);
+                            break;
+                        case 3:
+                            valid = ValidationThree(mes);
+                            break;
+
+                    }
+                    if (!valid)
+                    {
+                        mes.Text = "Не вірно введені дані "+textArrDialogRegister[--state];
+
+                      //  dialogTwoState[mes.DialogId]--;
+                        return mes;
+
+                    }
+                    mes.Text = textArrDialogRegister[state];
+                    dialogTwoState[mes.DialogId]++;
+
+                    if (textArrDialogRegister.Length <= dialogTwoState[mes.DialogId])
+                    {
+                        clientDictionary[mes.DialogId] = 0;
+                        dialogTwoState[mes.DialogId] = 0;
+                      
+                    }
+                    return mes;
+
+                }
 
 
 
+              
 
+
+            }
+            else
+            {
+                dialogTwoState.Add(mes.DialogId, 0);
+                
+          
+
+            }
+            mes.Text = textArrDialogRegister[dialogTwoState[mes.DialogId]];
+            dialogTwoState[mes.DialogId]++;
 
             return mes;
+
+
+            bool ValidationOne (Message mes)
+            {
+                if(dialogTwoState[mes.DialogId] ==1)
+                {
+                    var numberBooking = mes.Text;
+                   if(arrNumberBooking.Contains(numberBooking))
+                    {
+                       if(!UserDataDictionary.ContainsKey(mes.DialogId))
+                        {
+                            UserDataDictionary.Add(mes.DialogId, new Dictionary<string, string>() { { "NumBooking", mes.Text } });
+                            return true;
+                        }
+                       else
+                        {
+                            if(UserDataDictionary[mes.DialogId].ContainsKey("NumBooking"))
+                            {
+                                UserDataDictionary[mes.DialogId].Add("NumBooking", mes.Text);
+                            }
+                            else
+                            {
+                                UserDataDictionary[mes.DialogId]["NumBooking"] = mes.Text;
+                            } 
+                           
+                        }
+                       
+                    }
+                    else
+                    {
+                        return false;
+
+                    }
+
+                }
+
+                return false;
+
+            }
+
+            bool ValidationTwo(Message mes)
+            {
+                if (dialogTwoState[mes.DialogId] == 2)
+                {
+                    
+                    int numberseat;
+                    
+                    if (int.TryParse(mes.Text, out numberseat) && numberseat<=44 && numberseat>0)
+                    {
+                        if (UserDataDictionary[mes.DialogId].ContainsKey("NumBooking"))
+                        {
+                            UserDataDictionary[mes.DialogId].Add("NumSeat", mes.Text);
+                        }
+                        else
+                        {
+                            UserDataDictionary[mes.DialogId]["NumSeat"] = mes.Text;
+                        }
+                       
+                        
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+
+                    }
+
+                }
+
+                return false;
+
+            }
+
+            bool ValidationThree(Message mes)
+            {
+                if (dialogTwoState[mes.DialogId] == 3)
+                {
+
+                 
+
+                    if (UserDataDictionary[mes.DialogId]["NumBooking"] != null && UserDataDictionary[mes.DialogId]["NumSeat"] != null)
+                    {
+
+                      
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+
+                    }
+
+                }
+
+                return false;
+
+            }
+
+
         }
         public Message DialogThree(Message mes)
         {
@@ -183,6 +342,8 @@ namespace TechnicalSupport
             else
             {
                 mes.Text = "На даний момент немає доступних працівників";
+                clientDictionary[mes.DialogId] = 0;
+
             }
 
            
